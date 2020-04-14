ff15bf44b1e59c5cf89d91f15554fd94318d9a14
Leon Si <leonzalion@gmail.com>
2020-04-13 22:38:10 -0400

    fixedo

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const {ipcRenderer} = require('electron');
 const serverUrl = 'https://paranoia-server.herokuapp.com';
 const socket = io(serverUrl);
 
+socket.on('connect', function() {
+  console.log('connecetd');
+})
 socket.on('take-screenshot', async function() {
+  console.log('taking screenshot');
   const result = await ipcRenderer.invoke('screenshot');
-  socket.send('took-screenshot', result);
+  socket.emit('took-screenshot', result);
 });
