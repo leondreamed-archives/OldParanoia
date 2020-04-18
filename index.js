f6c5d82d0090bc254c95f48fb2aa5fc16cd2e29c
Leon Si <leonzalion@gmail.com>
2020-04-18 19:33:05 -0400

    updated cors

--- index.js
+++ index.js
@@ -3,14 +3,13 @@ const serverUrl = 'https://paranoia-server.herokuapp.com';
 const socket = io(serverUrl);
 
 socket.on('connect', function() {
-  socket.emit('client-connected');
+  console.log('hi');
+  socket.emit('create-room', 'leonzalion');
+  socket.on('take-screenshot', async function() {
+    console.log('taking screenshot');
+    const result = await ipcRenderer.invoke('screenshot');
+    socket.emit('took-screenshot', 'leonzalion', result);
+  });
 });
 
-socket.on('disconnect', function() {
-  socket.emit('client-disconnected');
-})
 
-socket.on('take-screenshot', async function() {
-  const result = await ipcRenderer.invoke('screenshot');
-  socket.emit('took-screenshot', result);
-});
